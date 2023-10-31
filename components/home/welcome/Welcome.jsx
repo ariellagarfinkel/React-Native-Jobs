e2d5413c8355f2a8e8636d927f85d9dbdba839fe
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-10-31 11:38:46 -0500

    adds broken search element to welcome page

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -15,8 +15,20 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}/>
+          <TextInput 
+            style={styles.searchInput} 
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
         </View>
+        <TouchableOpacity
+          <Image>
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        />  
       </View>
     </View>
   )
