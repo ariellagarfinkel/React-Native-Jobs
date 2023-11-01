75dd09ca4f6a013812ee27b0c1a84f2b33b44628
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-11-01 11:04:52 -0500

    add begininng of popular jobs and activity inidicator

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, Flatlist, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter
+  const isLoading = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+      <TouchableOpacity>
+        <Text style={styles.headerBtn}>Show All</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something Went Wrong</Text>
+        ) : (
+          <Flatlist
+            data={[1,2,3,4]}
+          />
+        )}
+      </View>
     </View>
   )
 }
