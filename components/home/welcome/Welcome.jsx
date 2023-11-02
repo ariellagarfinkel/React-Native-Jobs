f92ce46849a0d7344d4c0138818c52902c8c9925
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-11-02 11:45:14 -0500

    fake data for flatList keyextractor

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants';
 
-const jobTypes = ["Full-time", "Part-time", "Contractor" ]
+const jobTypes = ["Full-time", "Part-time", "Contractor", "Babysitter", "Morah", "Electrician", "lawnmower"]
 
 const Welcome = () => {
   const router = useRouter();
