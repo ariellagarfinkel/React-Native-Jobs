53dde22524ccf73a2c6716b93871d2f8b05bd07a
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-11-03 11:23:04 -0500

    create async funtions using try and catch block to fetch data from a third party api

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -19,4 +19,30 @@ const options = {
     },
     params: { ...query},
   };
-  
+
+const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+        const response = await axios.request(options);
+
+        setData(response.data.data)
+        setIsLoading(false);
+    }   catch (error) {
+        setError(error);
+        alert('There is an error')
+    }   finally {
+        setIsLoading(false);
+    }
+}
+
+useEffect(() => {
+    fetchData();
+}, []);
+
+const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+}
+
+return { data, isLoading, error, refetch };
