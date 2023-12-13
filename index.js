d3ec976ef880cecfb63d78365bd2df23b7c4b428
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-12-13 11:07:04 -0600

    trying again

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import "expo-router/entry";
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  // const ctx = require.context('./app');
+  const ctx = require.context('./app', true, /\.jsx?$/);
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
+
 
