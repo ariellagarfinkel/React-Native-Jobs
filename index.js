f56c44687aa12f659b0c8d79d43e5d1fae47c254
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-01-03 11:22:10 -0600

    replace underscores with dashes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { ExpoRoot } from 'expo-router';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  // const ctx = require.context('./app');
-  const ctx = require.context('./app', true, /\.jsx?$/);
+  const ctx = require.context('./app');
+  // const ctx = require.context('./app', true, /\.tsx?$/);
   return <ExpoRoot context={ctx} />;
 }
 
