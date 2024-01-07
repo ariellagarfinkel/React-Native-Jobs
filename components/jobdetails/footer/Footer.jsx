5c82491c6a3e7e81ffa3330d029efb9c8f02480d
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-01-07 13:24:39 -0600

    add like and apply button to footer

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native'
 import styles from './footer.style'
+import { icons } from '../../../constants';
 
 const Footer = () => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn} >
+        <Image
+          source={icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// import "expo-router/entry";
-// import { registerRootComponent } from 'expo';
-// import { ExpoRoot } from 'expo-router';
-
-// // Must be exported or Fast Refresh won't update the context
-// export function App() {
-//   const ctx = require.context('./app');
-//   // const ctx = require.context('./app', true, /\.tsx?$/);
-//   return <ExpoRoot context={ctx} />;
-// }
-
-// registerRootComponent(App);
-
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
