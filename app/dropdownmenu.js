6a7b811c412b96ed6ab36e7f0b4aaffaa2c88e00
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-01-18 11:10:43 -0600

    add navigation to the buttons on dropdown menu

--- app/dropdownmenu.js
+++ app/dropdownmenu.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './dropdownmenu.style';
+import { Signup } from '../components/account/signup';
+import { Login } from '../components/account/login';
+import { Logout } from '../components/account/logout';
 
 const DropdownMenu = ({ isVisible, toggleDropdown}) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +19,36 @@ const DropdownMenu = ({ isVisible, toggleDropdown}) => {
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
     toggleDropdown();
-  };
+
+  switch (option) {
+    case 'Signup':
+      navigation.navigate( <Signup />); 
+      break;
+    case 'Login':
+      navigation.navigate(<Login />); 
+      break;
+    case 'Logout':
+      navigation.navigate(<Logout />);
+      break;
+    // Add cases for other options (e.g., 'Favorites') if needed
+    default:
+      break;
+  }
+};
+
+  // const renderOptionComponent = () => {
+  //   switch (selectedOption) {
+  //     case 'Signup':
+  //       return <Signup />;
+  //     case 'Login':
+  //       return <Login />;
+  //     case 'Logout':
+  //       return <Logout />;
+  //     // Add cases for other options (e.g., 'Favorites') if needed
+  //     default:
+  //       return null;
+  //   }
+  // };
 
   return (
     <View style={{ padding: 20, display: isVisible ? 'flex' : 'none' }}>
